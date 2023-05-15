dd615cab3c53ae392c3eb1b6353d548bceea14d6
souravkumar1skp <2020ugee081@nitjsr.ac.in>
2023-05-16 00:36:56 +0530

    fog added

--- src/components/container.js
+++ src/components/container.js
@@ -6,8 +6,8 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const Map = () => {
   const mapContainer = useRef(null);
-  const [lng, setLng] = useState(77.378);
-  const [lat, setLat] = useState(28.624);
+  const [lng, setLng] = useState(0);
+  const [lat, setLat] = useState(0);
   // const [zoom, setZoom] = useState(1);
   
   useEffect(() => {
@@ -30,11 +30,19 @@ const Map = () => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lat, lng],
+      center: [0, 0],
       zoom: 1,
       projection: 'globe'
     });
-    
+    map.on('style.load', () => {
+      map.setFog({
+          color: 'rgb(186, 210, 235)', // Lower atmosphere
+          'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+          'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+          'space-color': 'rgb(11, 11, 25)', // Background color
+          'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+      });
+  });
     map.addControl(geolocateControl);
     
     return () => map.remove();
