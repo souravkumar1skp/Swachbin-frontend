50494b849a01eccea4aa180771705feac92073f8
Sourav Kumar <souravkumar1skp@gmail.com>
2023-07-14 18:15:36 +0000

    Update History.js

--- src/redux/Slice/History.js
+++ src/redux/Slice/History.js
@@ -1,14 +1,13 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-const historySlice= createSlice({
-    name: 'details',
-    initialState: [],
-    reducers: {
-        addItem : (state, action)=>
-        {
-            state= action.payload
-        }
-    }
+const historySlice = createSlice({
+  name: "details",
+  initialState: [],
+  reducers: {
+    addItem: (state, action) => {
+      state = action.payload;
+    },
+  },
 });
 
 export default historySlice.reducer;
